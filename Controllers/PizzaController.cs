7e68d9d7d913e836721d1fb7f0e6c65a3bac6e08
Kardast <dottorsf@gmail.com>
2022-11-21 14:54:57 +0100

    Viasualizzazione in pagina delle pizze

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,31 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            List<Pizza> listPizza = db.Pizzas.ToList();
+
+            //qualche altra cosa...
+
+            return View(listPizza);
+        }
+
+        //public IActionResult Detail(int id)
+        //{
+
+        //    BlogDbContext db = new BlogDbContext();
+
+        //    Post post = db.Posts.Where(p => p.Id == id).FirstOrDefault();
+
+        //    return View(post);
+        //}
+    }
+}
